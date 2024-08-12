8f340d96066d9cfc32f111706cf1147470e60af2
sdzx-1 <shangdizhixia1993@163.com>
2024-08-12 22:30:33 +0800

    cc

--- src/N.hs
+++ src/N.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE EmptyCase #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RankNTypes #-}
@@ -13,9 +14,12 @@ module N where
 
 import Control.Algebra (Has)
 import Control.Effect.Fresh
+import Control.Monad
+import Data.Kind (Constraint, Type)
+import Data.Traversable (for)
 import Data.Void
 
-data BranchSt eta r bst = BranchSt bst (M eta r bst)
+data BranchSt eta r bst = BranchSt bst (Protocol eta r bst)
   deriving (Functor)
 
 data MsgOrLabel eta r
@@ -23,24 +27,28 @@ data MsgOrLabel eta r
   | Label (XLabel eta) Int
   deriving (Functor)
 
-data M eta r bst
-  = (MsgOrLabel eta r) :> (M eta r bst)
-  | Branch (XBranch eta) r [BranchSt eta r bst]
-  | Goto (XBranch eta) Int
-  | Terminal (XBranch eta)
+infixr 5 :>
+
+data Protocol eta r bst
+  = (MsgOrLabel eta r) :> (Protocol eta r bst)
+  | Branch r [BranchSt eta r bst]
+  | Goto (XGoto eta) Int
+  | Terminal (XTerminal eta)
   deriving (Functor)
 
 type family XMsg eta
 type family XLabel eta
-type family XBranch eta
 type family XGoto eta
 type family XTerminal eta
 
+type ForallX (f :: Type -> Constraint) eta =
+  (f (XMsg eta), f (XLabel eta), f (XGoto eta), f (XTerminal eta))
+
+------------------------
 data Creat
 
 type instance XMsg Creat = ()
 type instance XLabel Creat = ()
-type instance XBranch Creat = ()
 type instance XGoto Creat = ()
 type instance XTerminal Creat = ()
 
@@ -48,7 +56,6 @@ data AddNums
 
 type instance XMsg AddNums = ([Int], [Int])
 type instance XLabel AddNums = [Int]
-type instance XBranch AddNums = [Int]
 type instance XGoto AddNums = [Int]
 type instance XTerminal AddNums = [Int]
 
@@ -58,7 +65,7 @@ addNums
      , Enum r
      , Bounded r
      )
-  => [Int] -> M Creat r bst -> m ([Int], M AddNums r bst)
+  => [Int] -> Protocol Creat r bst -> m ([Int], Protocol AddNums r bst)
 addNums inputNums = \case
   msgOrLabel :> ms -> do
     case msgOrLabel of
@@ -72,6 +79,80 @@ addNums inputNums = \case
       Label _ i -> do
         (is', ms') <- addNums inputNums ms
         pure (is', Label inputNums i :> ms')
-  Branch _ r ls -> undefined
-  Goto _ i -> undefined
-  Terminal _ -> undefined
+  Branch r ls -> do
+    (ins, ls') <- go inputNums ls
+    pure (ins, Branch r ls')
+  Goto _ i -> pure (inputNums, Goto inputNums i)
+  Terminal _ -> pure (inputNums, Terminal inputNums)
+
+go
+  :: forall r bst sig m
+   . ( Has Fresh sig m
+     , Enum r
+     , Bounded r
+     )
+  => [Int] -> [BranchSt Creat r bst] -> m ([Int], [BranchSt AddNums r bst])
+go inputNums = \case
+  [] -> pure (inputNums, [])
+  BranchSt bst prot : xs -> do
+    (_, prot') <- addNums inputNums prot
+    (ins'', branchs) <- go inputNums xs
+    pure (ins'', BranchSt bst prot' : branchs)
+
+----------------------------------
+
+ppProtocol :: (ForallX Show eta, Show r, Show bst) => Protocol eta r bst -> String
+ppProtocol = \case
+  msgOrLabel :> prot ->
+    let r1 = case msgOrLabel of
+          Msg xmsg a b c d ->
+            "Msg " ++ show xmsg ++ " " ++ a ++ " " ++ show b ++ " " ++ show c ++ " " ++ show d
+          Label xlabel i -> "Label " ++ show xlabel ++ " " ++ show i
+     in r1 ++ "\n" ++ ppProtocol prot
+  Branch r ls ->
+    "Branch "
+      ++ show r
+      ++ "\n"
+      ++ concatMap
+        ( \(BranchSt bst prot) ->
+            "BranchSt " ++ show bst ++ "\n" ++ ppProtocol prot
+        )
+        ls
+  Goto xv i -> "Goto " ++ show xv ++ " " ++ show i ++ "\n"
+  Terminal xv -> "Terminal " ++ show xv ++ "\n"
+
+----------------------------------
+data PingPong = Client | Server | Counter
+  deriving (Show, Eq, Ord, Enum, Bounded)
+
+data PPSt = PTrue | PFalse
+  deriving (Show)
+
+v1 :: Protocol Creat PingPong PPSt
+v1 =
+  Label () 0
+    :> Branch
+      Client
+      [ BranchSt PTrue $
+          Msg () "Ping" [] Client Server
+            :> Msg () "Pong" [] Server Client
+            :> Msg () "Add" [] Client Counter
+            :> Goto () 0
+      , BranchSt PFalse $
+          Msg () "Stop" [] Client Server
+            :> Msg () "AStop" [] Client Counter
+            :> Terminal ()
+      ]
+
+-- >>> error $ ppProtocol v1
+-- Label () 0
+-- Branch Client
+-- BranchSt PTrue
+-- Msg () Ping [] Client Server
+-- Msg () Pong [] Server Client
+-- Msg () Add [] Client Counter
+-- Goto () 0
+-- BranchSt PFalse
+-- Msg () Stop [] Client Server
+-- Msg () AStop [] Client Counter
+-- Terminal ()
