5a1d01a6785721599894ffc4416e6ecaa5b1526e
sdzx-1 <shangdizhixia1993@163.com>
2024-08-19 11:49:26 +0800

    Impl Parser

--- src/TypedSession/State/Parser.hs
+++ src/TypedSession/State/Parser.hs
@@ -1,15 +1,20 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -Wno-unused-do-bind #-}
+
 module TypedSession.State.Parser where
 
+import Data.Char (isUpper)
+import qualified Data.List as L
+import Text.Megaparsec hiding (Label, label)
+import Text.Megaparsec.Char (char, space1, string)
+import qualified Text.Megaparsec.Char.Lexer as L
+import Text.Megaparsec.Debug (dbg)
 import TypedSession.State.Pattern
-import TypedSession.State.Type (Creat, Protocol)
-
--- data PingPong
-
-data PingPongRole = Client | Server | Counter
-  deriving (Show, Read, Eq, Ord, Enum, Bounded)
-
-data PingPongBranchSt = STrue | SFalse
-  deriving (Show, Read, Eq, Ord, Enum, Bounded)
+import TypedSession.State.Type (BranchSt, Creat, MsgOrLabel, Protocol)
 
 {-
 
@@ -19,37 +24,145 @@ data PingPongBranchSt = STrue | SFalse
 
 -}
 
--- v1 :: Protocol Creat PingPongRole PingPongBranchSt
--- v1 =
---   Label 0
---     :> Branch
---       Client
---       [ BranchSt STrue $
---           Msg "Ping" ["Int", "Int", "Int"] Client Server
---             :> Msg "Pong" [] Server Client
---             :> Msg "AddOne" [] Client Counter
---             :> Goto 0
---       , BranchSt SFalse $
---           Msg "Stop" [] Client Server
---             :> Msg "CStop" [] Client Counter
---             :> Terminal
---       ]
+data ParserError
+  = EmptyInput
+  | TheFirstLetterNotCapitalized
+  deriving (Show, Eq, Ord)
 
-{-
+instance ShowErrorComponent ParserError where
+  showErrorComponent = \case
+    EmptyInput -> "Trying to parse constructor or type, but input is empty!"
+    TheFirstLetterNotCapitalized -> "Try to parse constructor or type, but the first letter is not capitalized!"
 
-pingpongProtocol = protocol @PingPongRole @PingPongBranchSt "PingPong"
+type Parser = Parsec ParserError String
 
-[pingpongProtocol |
-  Label 0
-    Branch Client
-       BranchSt STrue
-          Msg "Ping" ["Int", "Int", "Int"] Client Server
-          Msg "Pong" [] Server Client
-          Msg "AddOne" [] Client Counter
-          Goto 0
-       BranchSt SFalse
-          Msg "Stop" [] Client Server
-          Msg "CStop" [] Client Counter
-          Terminal
- |]
--}
+spaceConsumer :: Parser ()
+spaceConsumer =
+  L.space
+    space1
+    (L.skipLineComment "--")
+    (L.skipBlockCommentNested "{-" "-}")
+
+symbol :: String -> Parser String
+symbol = L.symbol spaceConsumer
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme spaceConsumer
+
+msg, label, branch, branchSt, goto, terminal :: Parser String
+msg = symbol "Msg"
+label = symbol "Label"
+branch = symbol "Branch"
+branchSt = symbol "BranchSt"
+goto = symbol "Goto"
+terminal = symbol "Terminal"
+
+integer :: Parser Integer
+integer = lexeme L.decimal
+
+comma :: Parser String
+comma = symbol ","
+
+brackets :: Parser a -> Parser a
+brackets = between (symbol "[") (symbol "]")
+
+braces :: Parser a -> Parser a
+braces = between (symbol "{") (symbol "}")
+
+constrOrType :: Parser String
+constrOrType = dbg "constrOrType" $ do
+  st <- char '"' >> manyTill L.charLiteral (char '"')
+  case st of
+    [] -> customFailure EmptyInput
+    (x : _) ->
+      if isUpper x
+        then pure st
+        else customFailure TheFirstLetterNotCapitalized
+  spaceConsumer
+  pure st
+
+mkParserA :: forall a. (Enum a, Bounded a, Show a) => Parser a
+mkParserA = do
+  let rg = [minBound @a .. maxBound]
+      rg' =
+        fmap snd
+          . L.sortBy (\(a, _) (b, _) -> compare b a)
+          $ zip (fmap (length . show) rg) rg
+  a <-
+    choice $ fmap (\r -> (string (show r) >> pure r)) rg'
+  spaceConsumer
+  pure a
+
+parseMsg
+  :: forall r
+   . (Enum r, Bounded r, Show r)
+  => Parser (MsgOrLabel Creat r)
+parseMsg = dbg "Msg" $ do
+  msg
+  constr <- constrOrType
+  args <- brackets (constrOrType `sepBy` comma)
+  from <- mkParserA @r
+  to <- mkParserA @r
+  pure $ Msg constr args from to
+
+parseLabel :: (Show r) => Parser (MsgOrLabel Creat r)
+parseLabel = dbg "Label" $ do
+  label
+  i <- fromIntegral <$> integer
+  pure $ Label i
+
+parseGoto :: (Show bst, Show r) => Parser (Protocol Creat r bst)
+parseGoto = dbg "Goto" $ do
+  goto
+  i <- fromIntegral <$> integer
+  pure $ Goto i
+
+parseTerminal :: (Show bst, Show r) => Parser (Protocol Creat r bst)
+parseTerminal = dbg "Terminal" $ do
+  terminal
+  pure $ Terminal
+
+parseBranchSt
+  :: forall bst r
+   . (Enum bst, Bounded bst, Show bst, Enum r, Bounded r, Show r)
+  => Parser (BranchSt Creat r bst)
+parseBranchSt = dbg "BranchSt" $ do
+  branchSt
+  bst <- mkParserA @bst
+  prot <- parseProtocol @r @bst
+  pure (BranchSt bst prot)
+
+parseBranch
+  :: forall r bst
+   . (Enum r, Bounded r, Show r, Enum bst, Bounded bst, Show bst) => Parser (Protocol Creat r bst)
+parseBranch = dbg "Branch" $ do
+  branch
+  r1 <- mkParserA @r
+  branchSts <- some (parseBranchSt @bst @r)
+  pure (Branch r1 branchSts)
+
+parseMsgOrLabel
+  :: forall r bst
+   . (Enum r, Bounded r, Show r, Enum bst, Bounded bst, Show bst)
+  => Parser (Protocol Creat r bst)
+parseMsgOrLabel = dbg "MsgOrLabel" $ do
+  msgOrLabel <- choice [parseMsg @r, parseLabel]
+  prot <- parseProtocol @r @bst
+  pure (msgOrLabel :> prot)
+
+parseProtocol
+  :: forall r bst
+   . (Enum r, Bounded r, Show r, Enum bst, Bounded bst, Show bst)
+  => Parser (Protocol Creat r bst)
+parseProtocol = dbg "Protocol" $ do
+  choice [parseGoto, parseTerminal, parseBranch @r @bst, parseMsgOrLabel]
+
+runProtocolParser
+  :: forall r bst
+   . (Enum r, Enum bst, Bounded r, Bounded bst, Show r, Show bst)
+  => String
+  -> Either
+      (ParseErrorBundle String ParserError)
+      (Protocol Creat r bst)
+runProtocolParser =
+  runParser (between spaceConsumer eof $ parseProtocol @r @bst) ""
--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,207 @@
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TypeApplications #-}
+
 module Main (main) where
+
+import Text.RawString.QQ (r)
+import TypedSession.State.Parser (runProtocolParser)
+import TypedSession.State.Pattern
+import TypedSession.State.Type (Creat, Protocol)
 
 main :: IO ()
 main = putStrLn "Test suite not yet implemented."
+
+data PingPongRole = Client | Server | Counter
+  deriving (Show, Read, Eq, Ord, Enum, Bounded)
+
+data PingPongBranchSt = STrue | SFalse
+  deriving (Show, Read, Eq, Ord, Enum, Bounded)
+
+v1 :: Protocol Creat PingPongRole PingPongBranchSt
+v1 =
+  Label 0
+    :> Branch
+      Client
+      [ BranchSt STrue $
+          Msg "Ping" ["Int", "Int", "Int"] Client Server
+            :> Msg "Pong" [] Server Client
+            :> Msg "AddOne" [] Client Counter
+            :> Goto 0
+      , BranchSt SFalse $
+          Msg "Stop" [] Client Server
+            :> Msg "CStop" [] Client Counter
+            :> Terminal
+      ]
+
+s1 =
+  [r|
+  
+  Label 0
+    Branch Client 
+      BranchSt STrue
+          Msg "Ping" ["Int", "Int", "Int"] Client Server
+          Msg "Pong" [] Server Client
+          Msg "AddOne" [] Client Counter
+          Goto 0
+      BranchSt SFalse
+          Msg "Stop" [] Client Server
+          Msg "CStop" [] Client Counter
+          Terminal
+|]
+
+r1 = runProtocolParser @PingPongRole @PingPongBranchSt s1
+
+-- >>> r1
+-- Right Label () 0
+-- [Branch] () Client
+--   * BranchSt () STrue
+--   Msg <()> Ping [Int, Int, Int] Client Server
+--   Msg <()> Pong [] Server Client
+--   Msg <()> AddOne [] Client Counter
+--   Goto () 0
+--   * BranchSt () SFalse
+--   Msg <()> Stop [] Client Server
+--   Msg <()> CStop [] Client Counter
+--   Terminal ()
+
+
+data Role
+  = Buyer
+  | Seller
+  | Buyer2
+  deriving (Show, Eq, Ord, Enum, Bounded)
+
+data BookBranchSt
+  = NotFound
+  | Found
+  | One
+  | Two
+  | Support
+  | NotSupport
+  | Enough
+  | NotEnough
+  deriving (Show, Eq, Ord, Enum, Bounded)
+
+v2 :: Protocol Creat Role BookBranchSt
+v2 =
+  Label 0
+    :> Msg "Title" ["String"] Buyer Seller
+    :> Branch
+      Seller
+      [ BranchSt NotFound $
+          Msg "NoBook" [] Seller Buyer
+            :> Msg "SellerNoBook" [] Buyer Buyer2
+            :> Goto 0
+      , BranchSt Found $
+          Msg "Price" ["Int"] Seller Buyer
+            :> Branch
+              Buyer
+              [ BranchSt One $
+                  Msg "OneAfford" [] Buyer Buyer2
+                    :> Msg "OneAccept" [] Buyer Seller
+                    :> Msg "OneDate" ["Int"] Seller Buyer
+                    :> Msg "OneSuccess" ["Int"] Buyer Buyer2
+                    :> Goto 0
+              , BranchSt Two $
+                  Msg "PriceToBuyer2" ["Int"] Buyer Buyer2
+                    :> Branch
+                      Buyer2
+                      [ BranchSt NotSupport $
+                          Msg "NotSupport1" [] Buyer2 Buyer
+                            :> Msg "TwoNotBuy" [] Buyer Seller
+                            :> Goto 0
+                      , BranchSt Support $
+                          Msg "SupportVal" ["Int"] Buyer2 Buyer
+                            :> Branch
+                              Buyer
+                              [ BranchSt Enough $
+                                  Msg "TwoAccept" [] Buyer Seller
+                                    :> Msg "TwoDate" ["Int"] Seller Buyer
+                                    :> Msg "TwoSuccess" ["Int"] Buyer Buyer2
+                                    :> Goto 0
+                              , BranchSt NotEnough $
+                                  Msg "TwoNotBuy1" [] Buyer Seller
+                                    :> Msg "TwoFailed" [] Buyer Buyer2
+                                    :> Terminal
+                              ]
+                      ]
+              ]
+      ]
+
+s2 =
+  [r|
+  Label 0
+    Msg "Title" ["String"] Buyer Seller
+    Branch Seller
+        BranchSt NotFound 
+          Msg "NoBook" [] Seller Buyer
+               Msg "SellerNoBook" [] Buyer Buyer2
+               Goto 0
+        BranchSt Found 
+          Msg "Price" ["Int"] Seller Buyer
+          Branch Buyer
+            BranchSt One 
+              Msg "OneAfford" [] Buyer Buyer2
+              Msg "OneAccept" [] Buyer Seller
+              Msg "OneDate" ["Int"] Seller Buyer
+              Msg "OneSuccess" ["Int"] Buyer Buyer2
+              Goto 0
+            BranchSt Two 
+              Msg "PriceToBuyer2" ["Int"] Buyer Buyer2
+              Branch Buyer2
+                BranchSt NotSupport 
+                  Msg "NotSupport1" [] Buyer2 Buyer
+                  Msg "TwoNotBuy" [] Buyer Seller
+                  Goto 0
+                BranchSt Support 
+                  Msg "SupportVal" ["Int"] Buyer2 Buyer
+                  Branch Buyer
+                    BranchSt Enough 
+                      Msg "TwoAccept" [] Buyer Seller
+                      Msg "TwoDate" ["Int"] Seller Buyer
+                      Msg "TwoSuccess" ["Int"] Buyer Buyer2
+                      Goto 0
+                    BranchSt NotEnough 
+                      Msg "TwoNotBuy1" [] Buyer Seller
+                      Msg "TwoFailed" [] Buyer Buyer2
+                      Terminal
+|]
+
+r2 = runProtocolParser @Role @BookBranchSt s2
+
+-- >>> r2
+-- Right Label () 0
+-- Msg <()> Title [String] Buyer Seller
+-- [Branch] () Seller
+--   * BranchSt () NotFound
+--   Msg <()> NoBook [] Seller Buyer
+--   Msg <()> SellerNoBook [] Buyer Buyer2
+--   Goto () 0
+--   * BranchSt () Found
+--   Msg <()> Price [Int] Seller Buyer
+--   [Branch] () Buyer
+--     * BranchSt () One
+--     Msg <()> OneAfford [] Buyer Buyer2
+--     Msg <()> OneAccept [] Buyer Seller
+--     Msg <()> OneDate [Int] Seller Buyer
+--     Msg <()> OneSuccess [Int] Buyer Buyer2
+--     Goto () 0
+--     * BranchSt () Two
+--     Msg <()> PriceToBuyer2 [Int] Buyer Buyer2
+--     [Branch] () Buyer2
+--       * BranchSt () NotSupport
+--       Msg <()> NotSupport1 [] Buyer2 Buyer
+--       Msg <()> TwoNotBuy [] Buyer Seller
+--       Goto () 0
+--       * BranchSt () Support
+--       Msg <()> SupportVal [Int] Buyer2 Buyer
+--       [Branch] () Buyer
+--         * BranchSt () Enough
+--         Msg <()> TwoAccept [] Buyer Seller
+--         Msg <()> TwoDate [Int] Seller Buyer
+--         Msg <()> TwoSuccess [Int] Buyer Buyer2
+--         Goto () 0
+--         * BranchSt () NotEnough
+--         Msg <()> TwoNotBuy1 [] Buyer Seller
+--         Msg <()> TwoFailed [] Buyer Buyer2
+--         Terminal ()
